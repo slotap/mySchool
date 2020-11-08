ee2c09947730182399f6dc31f80344a38e14b6ae
Piotr Slota <slotap@gmail.com>
2020-11-08 16:16:02 +0100

    Keycloack configuration

---
+++ src/main/java/com/slotap/myschool/MySchoolApplication.java
@@ -0,0 +1,13 @@
+package com.slotap.myschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySchoolApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/slotap/myschool/security/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package com.slotap.myschool.security;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+@KeycloakConfiguration
+class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter {
+    @Bean
+   KeycloakSpringBootConfigResolver keycloakConfigResolver(){
+        return new KeycloakSpringBootConfigResolver();
+    }
+
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+}
